accb9254a55d3ee9ea747e84ac151cd2769ecb80
gurau-anil <gurau.anil@gmail.com>
2025-04-08 14:23:29 -0700

    Feature Management example1

---
+++ Example1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+using Example1.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using System.Net;
+
+namespace Example1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IFeatureManager _featureManager;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IFeatureManager featureManager)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("get-data")]
+        public async Task<IActionResult> GetData()
+        {
+            if (!await _featureManager.IsEnabledAsync("FeatureA"))
+            {
+                return new OkObjectResult("Feature A is not Enable")
+                {
+                    StatusCode = (int?)HttpStatusCode.Forbidden
+                };
+            }
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray());
+        }
+    }
+}
